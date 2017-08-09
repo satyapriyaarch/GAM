210d44639700ce1a7106b9018b554565aeb7614c
SATYA <satyapriyarajput@gmail.com>
2017-08-09 07:17:06 +0530

    Initial Commit

---
+++ MindIT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MindIT
+//
+//  Created by SATYA on 09/08/17.
+//  Copyright © 2017 Builder Infotech Pvt Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
