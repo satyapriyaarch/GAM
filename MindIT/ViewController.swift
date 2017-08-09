2c19995d4a613f66d3bfa0d101cbe7efadc3cc26
apple <satyapriya.arch@gmail.com>
2017-08-09 10:53:42 +0530

    Good

--- MindIT/ViewController.swift
+++ MindIT/ViewController.swift
@@ -9,10 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var imageView1: UIImageView!
+     @IBOutlet weak var imageView2: UIImageView!
+     @IBOutlet weak var imageView3: UIImageView!
+     @IBOutlet weak var imageView4: UIImageView!
+     @IBOutlet weak var imageView5: UIImageView!
+     @IBOutlet weak var imageView6: UIImageView!
+    @IBOutlet weak var imageView7: UIImageView!
+    @IBOutlet weak var imageView8: UIImageView!
+    @IBOutlet weak var imageView9: UIImageView!
+    @IBOutlet weak var imageView10: UIImageView!
+    @IBOutlet weak var imageView11: UIImageView!
+    @IBOutlet weak var imageView12: UIImageView!
+    @IBOutlet weak var imageView13: UIImageView!
+    @IBOutlet weak var imageView14: UIImageView!
+    @IBOutlet weak var imageView15: UIImageView!
+    @IBOutlet weak var imageView16: UIImageView!
+    @IBOutlet weak var imageView17: UIImageView!
+    @IBOutlet weak var imageView18: UIImageView!
+    @IBOutlet weak var imageView19: UIImageView!
+    @IBOutlet weak var imageView20: UIImageView!
+    @IBOutlet weak var imageView21: UIImageView!
+    @IBOutlet weak var imageView22: UIImageView!
+    @IBOutlet weak var imageView23: UIImageView!
+    @IBOutlet weak var imageView24: UIImageView!
+     @IBOutlet weak var imageView25: UIImageView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        self.view.backgroundColor = UIColor.init(patternImage: UIImage.init(named: "blue_bkgnd.png")!);
     }
 
     override func didReceiveMemoryWarning() {
