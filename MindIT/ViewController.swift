ab081fd97a703d93be04c62ace1d4900803a9622
apple <satyapriya.arch@gmail.com>
2017-08-09 18:03:37 +0530

    rgdfgdf

--- MindIT/ViewController.swift
+++ MindIT/ViewController.swift
@@ -8,6 +8,25 @@
 
 import UIKit
 
+extension Array {
+    mutating func shuffle() {
+        for _ in 0..<((count>0) ? (count-1) : 0) {
+            sort { (_,_) in arc4random() < arc4random() }
+        }
+    }
+}
+
+
+extension UIImage {
+    
+    func isEqualToImage(image: UIImage) -> Bool {
+        let data1: NSData = UIImagePNGRepresentation(self)! as NSData
+        let data2: NSData = UIImagePNGRepresentation(image)! as NSData
+        return data1.isEqual(data2)
+    }
+    
+}
+
 class ViewController: UIViewController {
     
     
@@ -35,15 +54,81 @@ class ViewController: UIViewController {
     @IBOutlet weak var imageView22: UIImageView!
     @IBOutlet weak var imageView23: UIImageView!
     @IBOutlet weak var imageView24: UIImageView!
-     @IBOutlet weak var imageView25: UIImageView!
+     @IBOutlet weak var countDown: UILabel!
+    var timerx :Timer = Timer()
     
-
+    var firstImage:UIImageView!
+    var secondImage:UIImageView!
+    var selectedImage:Int = 0;
+    var counter:Int = 0
+    
+    var imageArray:[String] = [];
+    var imageArrayCopy:[String] = [];
+    var imageViews:[UIImageView] = [];
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         
+        imageArray = ["img_1.png","img_2.png","img_3.png","img_4.png","img_5.png",
+                      "img_4.png","img_5.png","img_6.png","img_7.png",
+                      "img_8.png","img_7.png","img_9.png",
+                      "img_10.png","img_3.png","img_8.png","img_10.png",
+                      "img_6.png","img_2.png","img_9.png","img_1.png","img_13.png"]
+        
+        
+        
+        //Suffel imagenames
+        imageArrayCopy = imageArray;
+        
+        imageArrayCopy.shuffle()
+        
+        for imageName in imageArrayCopy{
+            
+            print(imageName)
+        }
+        
+        
+        imageViews = [imageView1,imageView2,imageView3,imageView4,imageView5,imageView6,imageView7,
+                      imageView8, imageView9,imageView10, imageView11,imageView12,imageView13,imageView14,imageView15,imageView16,imageView17,imageView18,imageView19,imageView20,imageView21]
+        
+        for (index,imageViewTemp) in imageViews.enumerated() {
+            
+            
+            imageViewTemp.image = UIImage.init(named: imageArrayCopy[index])
+           
+            imageViews[index].tag = index
+        }
+        
+        
+       // foldAllImages()
+        
         self.view.backgroundColor = UIColor.init(patternImage: UIImage.init(named: "blue_bkgnd.png")!);
+        
+        
+        selectedImage = 0;
+        Timer.scheduledTimer(withTimeInterval: 5.0, repeats: false, block: {_ in
+        
+        self.foldAllImages()
+        })
+     counter = 5
+        timerx =  Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: {
+            _ in
+            self.counter = self.counter  - 1
+            self.countDown.text =  "\(self.counter)"
+            
+            if self.countDown.text == "0" {
+                self.countDown.text = ""
+                self.timerx.invalidate()
+            }
+    
+        })
+       
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -52,6 +137,124 @@ class ViewController: UIViewController {
         
         
     }
+    
+    
+   @IBAction func foldAllImages()
+    {
+        
+        for (index,imageViewTemp) in imageViews.enumerated() {
+            
+            
+            
+            UIView.beginAnimations(nil, context: nil)
+            UIView.setAnimationDuration(3.0)
+            UIView.setAnimationTransition(.flipFromRight, for: imageViewTemp, cache:true)
+            UIView.commitAnimations()
+            
+            imageViewTemp.image = UIImage.init(named: "blank_card.png")!
+
+        }
+        
+        
+    }
+    
+    
+    @IBAction func FlipBothCards()    {
+        
+        UIView.beginAnimations(nil, context: nil)
+        UIView.setAnimationDuration(1.0)
+        UIView.setAnimationTransition(.flipFromRight, for: firstImage, cache:true)
+        UIView.commitAnimations()
+        
+        firstImage.image = UIImage.init(named: "blank_card.png")!
+        
+        UIView.beginAnimations(nil, context: nil)
+        UIView.setAnimationDuration(1.0)
+        UIView.setAnimationTransition(.flipFromRight, for: secondImage, cache:true)
+        UIView.commitAnimations()
+        
+        secondImage.image = UIImage.init(named: "blank_card.png")!
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+
+        
+        var touch :UITouch = touches.first as UITouch!
+        
+        var selectedTag:Int = (touch.view?.tag)!
+        
+        UIView.beginAnimations(nil, context: nil)
+        UIView.setAnimationDuration(1.0)
+        UIView.setAnimationTransition(.flipFromRight, for: self.imageViews[(touch.view?.tag)!], cache:true)
+        UIView.commitAnimations()
+        
+      ///  firstImage =  self.imageViews[(touch.view?.tag)!]
+        
+        self.imageViews[(touch.view?.tag)!].image = UIImage.init(named: imageArrayCopy[(touch.view?.tag)!])
+        
+        if (touch.view != self.view) {
+
+          if (selectedImage == 0)
+          {
+            selectedImage = 1
+          }
+            if selectedImage == 1 {
+                
+                
+//
+//                UIView.beginAnimations(nil, context: nil)
+//                UIView.setAnimationDuration(1.0)
+//                UIView.setAnimationTransition(.flipFromRight, for: self.imageViews[(touch.view?.tag)!], cache:true)
+//                UIView.commitAnimations()
+//                
+//                firstImage =  self.imageViews[(touch.view?.tag)!]
+//                
+//                self.imageViews[(touch.view?.tag)!].image = UIImage.init(named: imageArrayCopy[(touch.view?.tag)!])
+                
+                firstImage =  self.imageViews[(touch.view?.tag)!]
+                
+                 print("Card Touched first \(selectedTag))" )
+                
+                selectedImage = 2
+                
+                
+            }else if selectedImage == 2
+            {
+              
+                
+                UIView.beginAnimations(nil, context: nil)
+                UIView.setAnimationDuration(1.0)
+                UIView.setAnimationTransition(.flipFromRight, for: self.imageViews[(touch.view?.tag)!], cache:true)
+                UIView.commitAnimations()
+                
+                print("Card Touched second \(selectedTag))" )
+                secondImage =  self.imageViews[(touch.view?.tag)!]
+                
+                
+                
+                if (secondImage.image?.isEqualToImage(image: firstImage.image!))!
+                {
+                    print("success")
+                }else
+                {
+                    
+                    print("failed")
+                    
+                    self.FlipBothCards()
+                    
+                }
+                
+                  selectedImage = 1
+            }
+            
+            
+        }
+        
+    }
+ 
+    
+
 
 
 }
